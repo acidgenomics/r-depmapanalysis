acc8d87c7880620b566542a132241a03005bc436
Michael Steinbaugh <mike@steinbaugh.com>
2020-09-30 08:57:15 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,7 @@
-#' @importFrom BiocFileCache BiocFileCache bfcadd bfcdownload bfcneedsupdate
-#'   bfcquery bfcrpath
+#' @importFrom BiocFileCache BiocFileCache bfcadd bfccache bfcdownload
+#'   bfcneedsupdate bfcquery bfcrpath
+#' @importFrom cli cli_alert
+#' @importFrom goalie assert hasLength isAFile isAURL isFlag
 #' @importFrom rappdirs user_cache_dir
+#' @importFrom utils packageName
 NULL
