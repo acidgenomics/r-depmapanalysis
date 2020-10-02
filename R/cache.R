8a5a82908f42a3ea4d5e145a462f1ad9e9ee85c3
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-02 07:49:14 -0400

    Add common essential / nonessential files

--- R/cache.R
+++ R/cache.R
@@ -81,11 +81,21 @@
     "url_stem" = "https://ndownloader.figshare.com/files/",
     ## CRISPR screens.
     "20q3" = list(
+        ## List of genes identified as dependencies in all lines, one per line.
+        "achilles_common_essentials.csv" = "24613283",
         "achilles_gene_dependency.csv" = "24613298",
         "achilles_gene_effect.csv" = "24613292",
         "ccle_expression.csv" = "24613325",
         "ccle_gene_cn.csv" = "24613352",
         "ccle_mutations.csv" = "24613355",
+        ## List of genes used as positive controls, intersection of Biomen
+        ## (2014) and Hart (2015) essentials in the format "HUGO (Entrez)". Each
+        ## entry is separated by a newline.The scores of these genes are used as
+        ## the dependent distribution for inferring dependency probability.
+        "common_essentials.csv" = "24613385",
+        ## List of genes used as negative controls (Hart (2014) nonessentials)
+        ## in the format "HUGO (Entrez)". Each entry is separated by a newline.
+        "nonessentials.csv" = "24613388",
         "sample_info.csv" = "24613394"
     ),
     ## RNAi screens.
@@ -102,7 +112,7 @@
 #' @note Updated 2020-10-01.
 #' @noRd
 .importCellLineSampleData <-  # nolint
-    function(release = NULL) {
+    function(release) {
         df <- .importDataFile(
             fileName = "sample_info.csv",
             release = release,
