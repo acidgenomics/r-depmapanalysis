c5ce65f54d839eccd134f7a9a909cfd0dd83e454
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-10 10:28:22 -0500

    NAMESPACE rework

--- R/imports.R
+++ R/imports.R
@@ -1,19 +1,15 @@
-#' @importClassesFrom S4Vectors DataFrame
-#' @importClassesFrom SummarizedExperiment SummarizedExperiment
+#' @importClassesFrom basejump DataFrame RangedSummarizedExperiment
+#'   SummarizedExperiment
 #'
-#' @importFrom S4Vectors complete.cases mcols
-#' @importFrom SummarizedExperiment assay
-#' @importFrom basejump Ensembl2Entrez as_tibble cacheURL camelCase decode
-#'   geneSynonyms import makeDimnames makeGRangesFromEnsembl
-#'   makeSummarizedExperiment mapGenesToRownames melt snakeCase
-#' @importFrom cli cli_alert cli_alert_warning cli_dl
+#' @importFrom basejump Ensembl2Entrez alert alertWarning as_tibble assay
+#'   cacheURL camelCase complete.cases decode geneSynonyms import makeDimnames
+#'   makeGRangesFromEnsembl makeSummarizedExperiment mapGenesToRownames mcols
+#'   melt packageName packageVersion reorder snakeCase
 #' @importFrom ggplot2 aes coord_flip facet_wrap geom_boxplot geom_density
 #'   geom_hline geom_vline ggplot labs scale_x_discrete
 #' @importFrom goalie assert hasDimnames hasLength hasRownames isAFile isAURL
 #'   isCharacter isFlag isScalar isString isSubset validate validateClasses
 #' @importFrom methods as is new setClass setValidity validObject
 #' @importFrom rlang !! sym
-#' @importFrom stats reorder
 #' @importFrom stringr str_extract
-#' @importFrom utils packageName packageVersion
 NULL
