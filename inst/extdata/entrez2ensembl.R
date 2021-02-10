118039763942eda2a94d9f0f4beca9dcfb496258
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-10 12:36:20 -0500

    Rework Ensembl ID mapping

--- inst/extdata/entrez2ensembl.R
+++ inst/extdata/entrez2ensembl.R
@@ -4,47 +4,45 @@
 ## FIXME UPDATE THIS.
 
 library(basejump)        # 0.14.3
-library(tidyverse)       # 1.3.0
 library(DepMapAnalysis)  # 0.0.4
-
-organism <- "Homo sapiens"
+library(tidyverse)       # 1.3.0
 
 ## Download the latest version of the Achilles dataset.
-achilles <- Achilles(rowRanges = FALSE, colData = FALSE)
+achilles <- Achilles(
+    release = "21Q1",
+    rowRanges = FALSE,
+    colData = FALSE
+)
 
 ## Map the Entrez identifiers to Ensembl using NCBI OrgDb from AnnotationHub.
-entrez <- sort(as.integer(str_extract(
+entrezIds <- sort(as.integer(str_extract(
     string = rownames(achilles),
     pattern = "[0-9]+$"
 )))
 
 ## There are some Entrez IDs missing in the OrgDb that we need to map.
 ## Look up at https://www.ncbi.nlm.nih.gov/gene/
-manualEntrez <-
+manual <-
     import("manual-entrez-ids.csv") %>%
-    as_tibble() %>%
     camelCase(strict = TRUE) %>%
-    select(entrezId, ensemblId, retired) %>%
-    ## FIXME RETHINK THIS STEP?
-    rename(
-        entrez = entrezId,
-        ensembl = ensemblId
-    )
+    .[, c("entrezId", "ensemblId", "ensemblRetired")]
+colnames(manual)[colnames(manual) == "ensemblRetired"] <- "retired"
 
 ## Skipping the bad keys, let's map primarily using the OrgDb lookup.
-badKeys <- manualEntrez[["entrez"]]
-entrez2ensembl <- Entrez2Ensembl(
-    object = setdiff(entrez, badKeys),
-    organism = organism,
+keys <- setdiff(x = entrezIds, y = manual[["entrezId"]])
+## NOTE If you encounter match failures here, add them to the manual Entrez
+## CSV file above.
+orgdb <- Entrez2Ensembl(
+    object = keys,
+    organism = "Homo sapiens",
     format = "1:1"
-) %>%
-    as_tibble(rownames = NULL)
+)
 
 ## As a fall back, get the genomic ranges from Ensembl Ensdb, which also
 ## contains Ensembl-to-Entrez identifier mappings.
 ## > gr <- makeGRangesFromEnsembl(
 ## >     organism = organism,
-## >     release = 101L
+## >     release = 102L
 ## > )
 ## > ensembl2entrez <- Ensembl2Entrez(
 ## >     object = gr,
@@ -55,8 +53,15 @@ entrez2ensembl <- Entrez2Ensembl(
 ## FALSE
 
 ## Now we're ready to combine the automatic mappings with our manual ones.
-entrez2ensembl <-
-    bind_rows(entrez2ensembl, manualEntrez) %>%
+out <-
+    bind_rows(
+        as_tibble(orgdb, rownames = NULL),
+        as_tibble(manual, rownames = NULL)
+    ) %>%
     arrange_all() %>%
     as("DataFrame")
-saveData(entrez2ensembl, dir = ".")
+stopifnot(
+    !any(is.na(out[["entrezId"]])),
+    !any(is.na(out[["ensemblId"]]))
+)
+saveRDS(object = out, file = "entrez2ensembl.rds")
