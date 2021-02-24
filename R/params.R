3795f8284ca24bc8305a03d65ee2b26ec72312f1
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-24 11:48:56 -0500

    Documentation update

--- R/params.R
+++ R/params.R
@@ -7,6 +7,6 @@
 #' @param release `character(1)` or `NULL`.
 #'   DepMap quarterly release ID (e.g. `"20Q3"`).
 #'   If left `NULL`, defaults to latest supported release.
-#' @param rowRanges `logical(1)`.
+#' @param rowData `logical(1)`.
 #'   Include gene annotations as genomic ranges.
 NULL
