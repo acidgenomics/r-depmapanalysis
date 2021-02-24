46286851e6220cc6a5c939e29abdcf79da8f1b0c
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-24 07:43:39 -0500

    Resave

--- inst/extdata/entrez2ensembl.R
+++ inst/extdata/entrez2ensembl.R
@@ -1,6 +1,7 @@
 ## Internal Entrez-to-Ensembl identifier mappings.
 ## Updated 2021-02-24.
 
+library(goalie)          # 0.5.0
 library(basejump)        # 0.14.13
 library(DepMapAnalysis)  # 0.0.4
 
@@ -24,33 +25,32 @@ rm(achilles)
 ## Look up at https://www.ncbi.nlm.nih.gov/gene/
 manual <-
     import("entrez2ensembl.csv") %>%
+    as("DataFrame") %>%
     camelCase(strict = TRUE) %>%
     .[, c("entrezId", "ensemblId", "ensemblRetired")]
 colnames(manual)[colnames(manual) == "ensemblRetired"] <- "retired"
+manual[["source"]] <- "manual"
 
 ## Skipping the bad keys, let's map primarily using the OrgDb lookup.
 keys <- setdiff(x = entrezIds, y = manual[["entrezId"]])
 
 ## NOTE If you encounter match failures here, add them to the manual Entrez
 ## CSV file above.
-## FIXME THIS STEP IS GETTING STUCK WHEN RUNNING IN SCRIPT.
-## SOME SORT OF BIOCONDUCTOR NAMESPACE ISSUE?
-## FIXME NEED TO IMPROVE THE PROGRESS FOR THIS STEP.
 orgdb <- Entrez2Ensembl(
     object = keys,
     organism = "Homo sapiens",
     format = "1:1"
 )
+orgdb <- as(orgdb, "DataFrame")
+orgdb[["retired"]] <- FALSE
+orgdb[["source"]] <- "orgdb"
 
 ## Now we're ready to combine the automatic mappings with our manual ones.
-out <-
-    dplyr::bind_rows(
-        as_tibble(orgdb, rownames = NULL),
-        as_tibble(manual, rownames = NULL)
-    ) %>%
-    dplyr::arrange_all() %>%
-    as("DataFrame")
-stopifnot(
+assert(areDisjointSets(manual[["entrezId"]], orgdb[["entrezId"]]))
+out <- rbind(manual, orgdb)
+out[["source"]] <- as.factor(out[["source"]])
+out <- out[order(out[["entrezId"]]), , drop = FALSE]
+assert(
     !any(is.na(out[["entrezId"]])),
     !any(is.na(out[["ensemblId"]]))
 )
