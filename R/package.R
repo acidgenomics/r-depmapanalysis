50413915c2aa800bed22f35fd55b6e5f58bacc91
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-24 07:29:39 -0500

    NAMESPACE updates

--- R/package.R
+++ R/package.R
@@ -8,9 +8,10 @@
 #'   SummarizedExperiment
 #'
 #' @importFrom basejump Ensembl2Entrez alert alertWarning as_tibble assay
-#'   cacheURL camelCase complete.cases decode geneSynonyms import makeDimnames
-#'   makeGRangesFromEnsembl makeSummarizedExperiment mapGenesToRownames mcols
-#'   melt packageName packageVersion reorder snakeCase
+#'   cacheURL camelCase complete.cases decode do.call geneSynonyms import lapply
+#'   makeDimnames makeGRangesFromEnsembl makeSummarizedExperiment
+#'   mapGenesToRownames mcols melt metadata packageName packageVersion reorder
+#'   snakeCase t
 #' @importFrom ggplot2 aes coord_flip facet_wrap geom_boxplot geom_density
 #'   geom_hline geom_vline ggplot labs scale_x_discrete
 #' @importFrom goalie assert hasDimnames hasLength hasRownames isAFile isAURL
