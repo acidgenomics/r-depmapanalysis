38c14e31c21a6e01520cfbf2a60117c1dd35b871
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-25 11:35:28 -0500

    Prepare release

--- R/package.R
+++ R/package.R
@@ -7,9 +7,10 @@
 #' @importClassesFrom basejump DataFrame SummarizedExperiment
 #'
 #' @importFrom basejump EntrezGeneInfo alert alertWarning assay assayNames
-#'   as_tibble cacheURL camelCase complete.cases decode do.call geneSynonyms
-#'   import lapply makeDimnames makeSummarizedExperiment mapGenesToRownames
-#'   mcols melt metadata packageName packageVersion reorder rbind snakeCase t
+#'   as_tibble cacheURL camelCase colData complete.cases decode do.call
+#'   geneSynonyms import lapply makeDimnames makeSummarizedExperiment
+#'   mapGenesToRownames mcols melt metadata metadata<- packageName
+#'   packageVersion reorder rbind rowData snakeCase t
 #' @importFrom ggplot2 aes coord_flip facet_wrap geom_boxplot geom_density
 #'   geom_hline geom_vline ggplot labs scale_x_discrete
 #' @importFrom goalie assert hasDimnames hasLength hasRownames isAFile isAURL
