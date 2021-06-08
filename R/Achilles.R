f544dcc3e7387b7b11e683ec390cac4eb1f35861
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-08 15:49:14 -0400

    Rethink our object approach here

--- R/Achilles.R
+++ R/Achilles.R
@@ -1,6 +1,8 @@
 ## FIXME Consider making a Harmonia object, which combines Broad Achilles
 ## data along with Sanger SCORE data.
 
+## FIXME Consider renaming this, so we can use Harmonia data...
+
 
 
 #' Import Project Achilles CRISPR gene effect data
