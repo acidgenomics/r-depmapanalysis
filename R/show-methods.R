d9bf537da57c7aa87760994a4701d671b7e2c7a7
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-09 08:31:05 -0400

    Need to think about how to slot the metadata here consistently

---
+++ R/show-methods.R
@@ -0,0 +1,8 @@
+## FIXME Need to add custom print method support
+## scoringMethod
+## knockdown type: CRISPR or RNAi
+##
+## The type of data included:
+## - CRISPR: DepMap 21Q2 Public+Score
+## -RNAi: Achilles+DRIVE+Marcotte
+##
