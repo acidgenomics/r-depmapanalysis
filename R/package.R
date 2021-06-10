8beaeb028dfb305050a5a3bbb9de735f9ee40b6b
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-10 12:13:21 -0400

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -6,12 +6,12 @@
 
 #' @importClassesFrom basejump DataFrame SummarizedExperiment
 #'
-#' @importFrom basejump EntrezGeneInfo alert alertWarning assay assayNames
-#'   as_tibble cacheURL camelCase capture.output colData complete.cases decode
-#'   do.call encode geneSynonyms import lapply makeDimnames
-#'   makeSummarizedExperiment mapGenesToRownames mcols melt metadata metadata<-
-#'   packageName packageVersion pasteURL reorder rbind rowData showHeader
-#'   showSlotInfo snakeCase str_match t
+#' @importFrom basejump EntrezGeneInfo alert alertInfo alertWarning assay
+#'   assayNames as_tibble cacheURL camelCase capture.output colData
+#'   complete.cases decode do.call encode geneSynonyms import lapply
+#'   makeDimnames makeSummarizedExperiment mapGenesToRownames mcols melt
+#'   metadata metadata<- packageName packageVersion pasteURL reorder rbind
+#'   rowData showHeader showSlotInfo snakeCase str_match t
 #' @importFrom ggplot2 aes coord_flip facet_wrap geom_boxplot geom_density
 #'   geom_hline geom_vline ggplot labs scale_x_discrete
 #' @importFrom goalie areIntersectingSets assert hasDimnames hasLength
