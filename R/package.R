7a115ef90a03d856bd4a571030d2cafa7c6385a0
Michael Steinbaugh <mike@steinbaugh.com>
2021-07-07 12:26:22 -0400

    Update documentation

--- R/package.R
+++ R/package.R
@@ -15,10 +15,21 @@
 #'   pasteURL reorder rbind rowData showHeader showSlotInfo split str_match t
 #' @importFrom ggplot2 aes facet_wrap geom_boxplot geom_density geom_jitter
 #'   geom_point geom_violin geom_vline ggplot labs scale_y_discrete
-#' @importFrom goalie areIntersectingSets assert bapply hasDimnames hasLength
-#'   hasRownames isAFile isAURL isCharacter isFlag isInt isPositive isScalar
-#'   isString isSubset validate validateClasses
+#' @importFrom goalie allAreURLs areIntersectingSets assert bapply hasDimnames
+#'   hasLength hasRownames isAFile isAURL isCharacter isFlag isInt isPositive
+#'   isScalar isString isSubset validate validateClasses
 #' @importFrom methods as is new setClass setValidity show validObject
 #' @importFrom rlang !! sym
 #' @importFrom tidytext reorder_within scale_y_reordered
 "_PACKAGE"
+
+
+
+#' @name params
+#' @inherit AcidRoxygen::params return title
+#' @keywords internal
+#'
+#' @param dataset `character(1)` or `NULL`.
+#'   DepMap dataset release name.
+#'   If left `NULL`, defaults to latest supported dataset.
+NULL
--- R/params.R
+++
@@ -1,8 +0,0 @@
-#' @name params
-#' @inherit AcidRoxygen::params return title
-#' @keywords internal
-#'
-#' @param dataset `character(1)` or `NULL`.
-#'   DepMap dataset release name.
-#'   If left `NULL`, defaults to latest supported dataset.
-NULL
