1e0a6d5e0446e6f846852dc0e025f30b8b408ebc
Michael Steinbaugh <mike@steinbaugh.com>
2021-07-19 13:14:59 -0400

    Pass lintr checks

--- R/Codependencies.R
+++ R/Codependencies.R
@@ -14,38 +14,39 @@
 #'
 #' @examples
 #' x <- Codependencies(gene = "SOX10")
-Codependencies <- function(
-    gene,
-    dataset = c(
-        "Chronos_Combined",
-        "CERES_Combined",
-        "RNAi_merged"
-    )
-) {
-    assert(isString(gene))
-    dataset <- match.arg(dataset)
-    url <- pasteURL(
-        "depmap.org",
-        "portal",
-        "gene",
+Codependencies <-  # nolint
+    function(
         gene,
-        paste0(
-            "top_correlations?",
-            "dataset_name=", dataset
-        ),
-        protocol = "https"
-    )
-    tmpfile <- .cacheURL(url)
-    df <- import(file = tmpfile, format = "csv")
-    df <- as(df, "DataFrame")
-    colnames(df) <- camelCase(colnames(df), strict = TRUE)
-    dataset <- df[["dataset"]][[1L]]
-    df[["dataset"]] <- NULL
-    metadata(df) <- list(
-        "dataset" = dataset,
-        "gene" = gene,
-        "packageName" = .pkgName,
-        "packageVersion" = .pkgVersion
-    )
-    new(Class = "Codependencies", df)
-}
+        dataset = c(
+            "Chronos_Combined",
+            "CERES_Combined",
+            "RNAi_merged"
+        )
+    ) {
+        assert(isString(gene))
+        dataset <- match.arg(dataset)
+        url <- pasteURL(
+            "depmap.org",
+            "portal",
+            "gene",
+            gene,
+            paste0(
+                "top_correlations?",
+                "dataset_name=", dataset
+            ),
+            protocol = "https"
+        )
+        tmpfile <- .cacheURL(url)
+        df <- import(file = tmpfile, format = "csv")
+        df <- as(df, "DataFrame")
+        colnames(df) <- camelCase(colnames(df), strict = TRUE)
+        dataset <- df[["dataset"]][[1L]]
+        df[["dataset"]] <- NULL
+        metadata(df) <- list(
+            "dataset" = dataset,
+            "gene" = gene,
+            "packageName" = .pkgName,
+            "packageVersion" = .pkgVersion
+        )
+        new(Class = "Codependencies", df)
+    }
