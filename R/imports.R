b8548675e5670f5a2b15c6d6e5b9ea67ee557340
Michael Steinbaugh <mike@steinbaugh.com>
2022-08-19 08:53:50 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -8,8 +8,8 @@ NULL
 
 ## S4 generics and methods =====================================================
 
-#' @importFrom AcidGenerics camelCase encode leftJoin makeDimnames
-#' mapGenesToRownames melt showHeader
+#' @importFrom AcidGenerics camelCase droplevels2 encode factorize leftJoin
+#' makeDimnames mapGenesToRownames melt showHeader
 #' @importFrom BiocGenerics %in% do.call lapply mean order rbind t which
 #' @importFrom IRanges median
 #' @importFrom S4Vectors complete.cases decode head mcols metadata metadata<-
@@ -19,9 +19,9 @@ NULL
 #' @importFrom pipette import
 #'
 #' @importMethodsFrom AcidBase showHeader
-#' @importMethodsFrom AcidExperiment mapGenesToRownames melt
+#' @importMethodsFrom AcidExperiment droplevels2 mapGenesToRownames melt
 #' @importMethodsFrom AcidPlyr leftJoin melt
-#' @importMethodsFrom pipette import
+#' @importMethodsFrom pipette factorize import
 #' @importMethodsFrom syntactic camelCase makeDimnames
 NULL
 
