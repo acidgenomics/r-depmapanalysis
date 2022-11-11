a601233cc17da688b63cc6fdf870f3a3709bed63
Michael Steinbaugh <mike@steinbaugh.com>
2022-11-11 13:38:19 -0500

    Resolve FIXMEs

--- R/internal-import.R
+++ R/internal-import.R
@@ -10,39 +10,13 @@
 
 
 
-## FIXME This is currently erroring with DEMETER2 dataset...
-
-## FIXME Now we're hitting this issue...need to rethink our GeneEffect
-## dataset import approach, arg....
-## → Importing /Users/mike/.cache/R/DepMapAnalysis/145d6f7b73c4_35020903 using data.table::`fread()`.
-## Error:
-##     ! Assert failure.
-## [1] isSubset(x = c("ccleId", "cellLineName"), y = colnames(cd[["x"]])) is
-## not TRUE.
-## Cause: `c("ccleId", "cellLineName")` has elements not in
-## `colnames(cd[["x"]])`: cellLineName
-## Backtrace:
-##     ▆
-## 1. └─DepMapAnalysis::GeneEffect("demeter2_data_v6")
-## 2.   └─DepMapAnalysis:::.makeSummarizedExperiment(...) at r-depmapanalysis/R/GeneEffect.R:91:8
-## 3.     └─DepMapAnalysis:::.standardizeDemeter2(se) at r-depmapanalysis/R/internal-SE.R:185:12
-## 4.       └─goalie::assert(...) at r-depmapanalysis/R/internal-SE.R:206:4
-## 5.         └─AcidCLI (local) stop(...)
-## 6.           └─cli::cli_abort(x, call = NULL)
-## 7.             └─rlang::abort(...)
-
-
-
 #' Import cell line sample metadata
 #'
 #' Sample metadata now indicates that there are merged cells we should drop
 #' from analysis (e.g. ACH-002260).
 #'
-#' @note Updated 2022-09-21.
+#' @note Updated 2022-11-08.
 #' @noRd
-#'
-#' @seealso
-#' - `.standardizeDemeter2` for DEMETER2 RNAi dataset handling.
 .importCellLineSampleData <- # nolint
     function(dataset) {
         url <- datasets[[dataset]][["files"]][["sample_info"]][["url"]]
@@ -52,31 +26,6 @@
             return = "DataFrame"
         )
         assert(is(df, "DataFrame"))
-        if (identical(dataset, "demeter2_data_v6")) {
-            return(df)
-        }
-        ## DEMETER2:
-        ##  [1] "CCLE_ID"                         "disease"
-        ##  [3] "disease_subtype"                 "disease_sub_subtype"
-        ##  [5] "in_DRIVE"                        "in_Achilles"
-        ##  [7] "in_Marcotte"                     "Novartis_name"
-        ##  [9] "Novartis_Primary_site"           "Novartis_Pathologist_Annotation"
-        ## [11] "Marcotte_name"                   "Marcotte_subtype_three_receptor"
-        ## [13] "Marcotte_subtype_neve"           "Marcotte_subtype_intrinsic"
-        ## FIXME Need to rethink this for DEMETER2.
-        ## FIXME We need to merge in the canonical metadata from DepMap...
-        assert(
-            isSubset(
-                x = c(
-                    "Cellosaurus_NCIt_disease",
-                    "Cellosaurus_NCIt_id",
-                    "COSMICID",
-                    "DepMap_ID",
-                    "RRID"
-                ),
-                y = colnames(df)
-            )
-        )
         colnames(df)[colnames(df) == "Cellosaurus_NCIt_disease"] <-
             "ncitDiseaseName"
         colnames(df)[colnames(df) == "Cellosaurus_NCIt_id"] <-
@@ -93,14 +42,15 @@
         }
         if (
             !isSubset("cellLineName", colnames(df)) &&
-                isSubset("strippedCellLineName", colnames(df))
+            isSubset("strippedCellLineName", colnames(df))
         ) {
             ## e.g. "depmap_public_20q3".
             df[["cellLineName"]] <- df[["strippedCellLineName"]]
         } else if (
             !isSubset("cellLineName", colnames(df)) &&
-                isSubset("ccleId", colnames(df))
+            isSubset("ccleId", colnames(df))
         ) {
+            ## e.g. "demeter2_data_v6".
             x <- df[["ccleId"]]
             x <- vapply(
                 X = strsplit(x = x, split = "_", fixed = TRUE),
@@ -156,7 +106,7 @@
             31316011L,
             35020903L
         )
-        if (as.integer(basename(url)) %in% malformedIds) {
+        if (isSubset(as.integer(basename(url)), malformedIds)) {
             requireNamespaces("data.table")
             engine <- "data.table"
         }
