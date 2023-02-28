0f2584f4d4b51683e757d85ba388fe6b89c80b63
Michael Steinbaugh <mike@steinbaugh.com>
2023-02-28 17:22:23 -0500

    Draft update to UniProtKB mapping code

--- R/internal-uniprot.R
+++ R/internal-uniprot.R
@@ -1,8 +1,65 @@
+## FIXME A2A2Z9 maps to ANKRD18B but is failing here...argh.
+## Seems like the org.Hs.eg.db annotations are out of date / problematic.
+
+
+
 #' Import UniProtKB identifier mappings
 #'
 #' @note Updated 2023-02-28.
 #' @noRd
-.importUniprot <- function() {
+#'
+#' @seealso
+#' - https://support.bioconductor.org/p/71702/
+.uniprotToGene <- function(ids) {
+    assert(
+        requireNamespaces("org.Hs.eg.db"),
+        isCharacter(ids)
+    )
+    org <- org.Hs.eg.db::org.Hs.eg.db
+    df <- data.frame(
+        "uniprotId" = ids,
+        "uniprotId2" = sub(pattern = "-.+$", replacement = "", x = ids)
+    )
+    suppressMessages({
+        map <- select(
+            x = org,
+            keys = unique(df[["uniprotId2"]]),
+            columns = c(
+                #"ENTREZID",
+                #"ENSEMBL",
+                "SYMBOL",
+                "GENENAME"
+            ),
+            keytype = "UNIPROT"
+        )
+    })
+    map <- map[complete.cases(map), ]
+    idx <- order(
+        map[["UNIPROT"]],
+        ##as.integer(map[["ENTREZID"]]),
+        ##map[["ENSEMBL"]]
+        map[["SYMBOL"]]
+    )
+    map <- map[idx, ]
+    idx <- which(!duplicated(map[["UNIPROT"]]))
+    map <- map[idx, ]
+    colnames(map) <- c(
+        "uniprotId2",
+        "entrezId",
+        "ensemblId",
+        "geneName",
+        "geneDescription"
+    )
+    xxx <- leftJoin(
+        as(df, "DataFrame"),
+        as(map, "DataFrame"),
+        by = "uniprotId2"
+    )
+    xxx[["uniprotId2"]] <- NULL
+
+
+
+
     ## FIXME Ugh this file is huge, is there another way?
     url <- pasteURL(
         "ftp.uniprot.org",
