c0d5842fcd2e4c82ef3e068b232410a2c0a56812
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-01 08:24:02 -0500

    Version pin this file and add FIXMEs

--- R/internal-sanger.R
+++ R/internal-sanger.R
@@ -3,20 +3,29 @@
 ## FIXME rename modelName to cellLineName
 ## FIXNE rename cancerTypeNcitId
 ## FIXME rename broadId to depmapId
+## FIXME Split the synonyms by semicolon.
 
 
+
+#' Import Sanger CellModelPassports cell line metadata
+#'
+#' @note Updated 2023-03-01.
+#' @noRd
+#'
+#' @details
+#' Alternatively, can use "model_list_latest.csv.gz".
 .importSangerMetadata <- function() {
+
     url <- pasteURL(
         "cog.sanger.ac.uk",
         "cmp",
         "download",
-        "model_list_latest.csv.gz",
+        "model_list_20230110.csv",
         protocol = "https"
     )
     con <- .cacheURL(url)
     df <- import(con = con, format = "csv", engine = "data.table")
     df <- as(df, "DataFrame")
     colnames(df) <- camelCase(colnames(df))
-    ## FIXME Split the synonyms by semicolon.
     df
 }
