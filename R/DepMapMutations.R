40d63a02b0e6a3d2cde6497c24210558bb5a8a0a
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-02 11:25:16 -0400

    Draft update to mutation sanitization

--- R/DepMapMutations.R
+++ R/DepMapMutations.R
@@ -26,12 +26,50 @@ DepMapMutations <- # nolint
             x = colnames(df)
         )
         colnames(df) <- camelCase(colnames(df), strict = TRUE)
-        ## FIXME Need to split these columns:
-        ## - associatedWith (by `;`).
-        ## - cancerMolecularGenetics -- funky (e.g. `""Dom, Rec""`).
-        ## - cosmicOverlappingMutations (by ` `).
-        ## - dbsnpId (by ` `).
-        ## - lineageAssociation -- funky (e.g. `""E, L, M""`).
+        sanitize <- function(x) {
+            ## Remove unnecessary quoting.
+            x <- sub(
+                pattern = "^[\"]+",
+                replacement = "",
+                x = x
+            )
+            x <- sub(
+                pattern = "[\"]+$",
+                replacement = "",
+                x = x
+            )
+            ## Fix malformed ";".
+            x <- gsub(
+                pattern = "%3B",
+                replacement = ";",
+                x = x,
+                fixed = TRUE
+            )
+            x <- sub(pattern = paste0(";$"), replacement = "", x = x)
+            ## Remove trailing spaces.
+            x <- sub(pattern = "[[:space:]]+$", replacement = "", x = x)
+            x
+        }
+        split <- function(x, sep) {
+            x <- strsplit(x = x, split = sep, fixed = TRUE)
+            x <- CharacterList(x)
+            x
+        }
+        df[["associatedWith"]] <- sanitize(df[["associatedWith"]])
+        df[["associatedWith"]] <- split(df[["associatedWith"]], sep = ";")
+        df[["cancerMolecularGenetics"]] <- sanitize(df[["associatedWith"]])
+        df[["cosmicOverlappingMutations"]] <-
+            split(df[["cosmicOverlappingMutations"]], sep = " ")
+        df[["dbsnpId"]] <- split(df[["dbsnpId"]], sep = " ")
+        df[["lineageAssociation"]] <- sanitize(df[["lineageAssociation"]])
+        df[["lineageAssociation"]] <- gsub(
+            pattern = ";",
+            replacement = ", ",
+            x = df[["lineageAssociation"]],
+            fixed = TRUE
+        )
+        df[["lineageAssociation"]] <-
+            split(df[["lineageAssociation"]], sep = ", ")
         ## - structuralRelation -- funky (e.g. `""ABL1,  FGFR1, JAK2 ""`).
         ##     Note that some of these have two spaces, so need to sanitize.
         ## - transcriptLikelyLof (by `;`).
