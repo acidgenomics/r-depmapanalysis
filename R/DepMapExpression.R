01065fc8a8146e080547ad69c31a3b0328740fb6
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-09 13:54:12 -0400

    Add FIXMEs regarding CPU and RAM checks

--- R/DepMapExpression.R
+++ R/DepMapExpression.R
@@ -35,11 +35,17 @@ DepMapGeneExpression <- # nolint
 ## FIXME Add a RAM check here, and inform user that this can take a long time.
 ## FIXME This uses that funky Profiles.csv file for identifiers, need to
 ## rethink the mapping.
+## FIXME Consider adding CPU core and RAM checks into goalie.
 
 #' @rdname DepMapExpression
 #' @export
 DepMapTxExpression <- # nolint
     function() {
+        ## FIXME Add this as new assert check to goalie. Super useful.
+        assert(
+            AcidBase::ram() >= 24L,
+            msg = "This function requires more RAM."
+        )
         dataset <- .currentBroadDataset
         ## FIXME Consider erroring unless user has > 16 GB of RAM.
         ## FIXME Need to import "OmicsProfiles.csv" here.
