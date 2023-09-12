12c3080d901b7ea0fcc6509108197d9995e3dd4f
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-12 12:38:58 -0400

    Migrate generics to AcidGenerics

--- R/imports.R
+++ R/imports.R
@@ -10,7 +10,9 @@ NULL
 
 #' @importFrom AcidGenerics autopadZeros camelCase diffExp droplevels2 encode
 #' excludeContaminatedCells factorize leftJoin makeDimnames makeNames
-#' mapGenesToRownames melt predictSensitivity showHeader tpm zscore
+#' mapGenesToRownames melt predictSensitivity plotGeneEffect
+#' plotGeneEffectVsExpression plotTopGeneEffectPerCell plotTopGeneEffectPerGroup
+#' selectCells showHeader tpm zscore
 #' @importFrom BiocGenerics %in% do.call lapply mean order rbind t which
 #' @importFrom IRanges gsub median sub
 #' @importFrom S4Vectors I complete.cases decode head mcols metadata metadata<-
