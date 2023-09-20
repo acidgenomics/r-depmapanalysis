c7d81ec8af797771713d81a59b90ed32c5921082
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-20 16:04:57 -0400

    Rework namespace

--- R/package.R
+++ R/package.R
@@ -5,3 +5,71 @@
 #' @aliases NULL
 #' @keywords internal
 "_PACKAGE"
+
+
+
+## S4 classes ==================================================================
+
+#' @importClassesFrom S4Vectors DFrame
+#' @importClassesFrom SummarizedExperiment RangedSummarizedExperiment
+#' SummarizedExperiment
+NULL
+
+
+
+## S4 generics and methods =====================================================
+
+#' @importFrom AcidGenerics as.DataFrame autopadZeros camelCase diffExp
+#' droplevels2 encode excludeContaminatedCells leftJoin makeDimnames
+#' makeNames mapGenesToRownames melt predictSensitivity plotGeneEffect
+#' plotGeneEffectVsExpression plotTopGeneEffectPerCell plotTopGeneEffectPerGroup
+#' selectCells showHeader tpm zscore
+#' @importFrom BiocGenerics %in% do.call lapply mean order rbind t which
+#' @importFrom IRanges gsub median sub
+#' @importFrom S4Vectors I complete.cases decode head mcols metadata metadata<-
+#' split unname
+#' @importFrom SummarizedExperiment assay assayNames colData colData<- rowData
+#' rowRanges
+#' @importFrom methods show
+#' @importFrom pipette import
+NULL
+
+#' @importMethodsFrom AcidBase showHeader
+#' @importMethodsFrom AcidExperiment droplevels2 mapGenesToRownames melt
+#' @importMethodsFrom AcidPlyr leftJoin melt
+#' @importMethodsFrom pipette import
+#' @importMethodsFrom syntactic autopadZeros camelCase makeDimnames makeNames
+NULL
+
+
+
+## S3 generics =================================================================
+
+#' @importFrom stats reorder
+NULL
+
+
+
+## Standard functions ==========================================================
+
+#' @importFrom AcidBase pasteURL showSlotInfo
+#' @importFrom AcidCLI abort alert alertInfo alertWarning dl h1 toInlineString
+#' @importFrom AcidExperiment makeSummarizedExperiment
+#' @importFrom AcidGenomes HGNC NcbiGeneInfo emptyRanges makeGRangesFromEnsembl
+#' @importFrom AcidPlots .data acid_geom_label_repel
+#' @importFrom Cellosaurus Cellosaurus
+#' @importFrom IRanges CharacterList
+#' @importFrom S4Vectors DataFrame
+#' @importFrom ggplot2 aes facet_wrap geom_boxplot geom_density geom_jitter
+#' geom_point geom_violin geom_vline ggplot labs scale_y_discrete
+#' @importFrom goalie allAreMatchingFixed allAreMatchingRegex allAreURLs
+#' areDisjointSets areIntersectingSets areSetEqual assert bapply hasColnames
+#' hasDuplicates hasLength hasNoDuplicates hasRAM hasRownames isAFile isAURL
+#' isCharacter isFlag isInt isInstalled isPositive isScalar isString isSubset
+#' requireNamespaces validate validateClasses
+#' @importFrom matrixStats colSums2 rowMaxs rowMins
+#' @importFrom methods as is new setClass setValidity validObject
+#' @importFrom pipette cacheURL
+#' @importFrom stringi stri_match_first_regex
+#' @importFrom utils capture.output packageName packageVersion
+NULL
